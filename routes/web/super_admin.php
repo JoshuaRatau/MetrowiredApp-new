3257ccbb426f38fa0be30407bcf738955c57b3cd
lebogangMatlala <lebogangmatlala94@gmail.com>
2023-02-28 14:04:56 +0200

    first commit

---
+++ routes/web/super_admin.php
@@ -0,0 +1,21 @@
+<?php
+
+// use Illuminate\Routing\Route;
+// use Illuminate\Support\Facades\Route as FacadesRoute;
+
+// Route::prefix('super/admin/')->group(function () {
+//     Route::get('/home', function () {
+//         return view('super_admin.home');
+//     }
+//     )->name('super_admin.home');
+
+// });
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('super/admin/')->group(function () {
+    Route::get('/home', function () {
+        return view('super_admin.home');
+    }
+    )->name('super_admin.home');
+});
